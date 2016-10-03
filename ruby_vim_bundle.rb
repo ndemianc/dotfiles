11e4dad22a8eacf7ed14a20450c02b1943fbe149
Sergii Demianchuk <sdemian@softserveinc.com>
2016-10-03 14:07:49 +0300

    Add a Ruby file that will download specified vim bundles inside ./bundle folder

---
+++ ruby_vim_bundle.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+git_bundles = [
+  'git@github.com:rking/ag.vim.git',
+  'git@github.com:ctrlpvim/ctrlp.vim.git',
+  'git@github.com:yegappan/mru.git',
+  'git@github.com:scrooloose/syntastic.git',
+  'git@github.com:tpope/vim-commentary.git',
+  'git@github.com:tpope/vim-fugitive.git',
+  'git@github.com:tpope/vim-rails.git',
+  'git@github.com:ecomba/vim-ruby-refactoring.git',
+  'git@github.com:vim-ruby/vim-ruby.git',
+  'git@github.com:tpope/vim-surround.git'
+]
+
+require 'fileutils'
+require 'open-uri'
+
+bundles_dir = File.join(File.dirname(__FILE__), "bundle")
+
+unless File.directory?(bundles_dir)
+  FileUtils.mkdir_p(bundles_dir)
+end
+
+FileUtils.cd(bundles_dir)
+
+puts "REMOVING EVERYTHING INSIDE (#{bundles_dir})"
+Dir["*"].each {|d| FileUtils.rm_rf d }
+
+git_bundles.each do |url|
+  dir = url.split('/').last.sub(/\.git$/, '')
+  puts "  Unpacking #{url} into #{dir}"
+  `git clone #{url} #{dir}`
+  FileUtils.rm_rf(File.join(dir, ".git"))
+end
